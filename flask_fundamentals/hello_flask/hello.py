06666b3f13cd47d2690c9cd94675341ae08af786
Lex Myers <lex.myers@gmail.com>
2017-09-07 13:17:40 -0700

    trial files

---
+++ flask_fundamentals/hello_flask/hello.py
@@ -0,0 +1,19 @@
+from flask import Flask  # Import Flask to allow us to create our app.
+from flask import render_template
+app = Flask(__name__)    # Global variable __name__ tells Flask whether or not we are running the file
+                         # directly, or importing it as a module.
+@app.route('/')          # The "@" symbol designates a "decorator" which attaches the following
+                         # function to the '/' route. This means that whenever we send a request to
+                         # localhost:5000/ we will run the following "hello_world" function.
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response.
+
+@app.route('/display')
+def display():
+    return render_template('index.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+app.run(debug=True)      # Run the app in debug mode.
