1f654e8e57f0fe25daad40147d8741bb811377f0
Lex Myers <lex.myers@gmail.com>
2017-09-08 13:09:27 -0700

    added win/lose/tie message

--- flask_fundamentals/rock-pap-sci/server.py
+++ flask_fundamentals/rock-pap-sci/server.py
@@ -7,25 +7,34 @@ app.secret_key= 'thisisnotakey'
 
 @app.route('/')
 def main_route():
-    if not 'wins' in session:
+    if not 'mesg' in session:
         session['wins'] = 0
         session['losses'] = 0
         session['ties'] = 0
-    return render_template('index.html',wins=session['wins'],losses=session['losses'],ties=session['ties'])
+        session['mesg'] = ''
+    return render_template('index.html',wins=session['wins'],losses=session['losses'],ties=session['ties'],mesg=session['mesg'])
 
 @app.route('/process_play',methods=['POST'])
 def process():
+    print "here!"
     if request.method == 'POST':
         comp_val = random.randint(0,2)
         player_val = rps.index(request.form['play'])
         if comp_val == player_val:
             session['ties'] += 1
+            done = 'tie'
         elif (player_val - comp_val) % 3 == 1:
             session['wins'] += 1
+            done = 'win'
         else:
             session['losses'] += 1
+            done = 'LOSE'
         print "player plays:", request.form['play']
         print "comp plays:", rps[comp_val]
-        redirect('/')
+        mesg = 'The computer picked ' + rps[comp_val]
+        mesg += ' and you picked ' + request.form['play']
+        mesg += ', you ' + done + '!'
+        session['mesg'] = mesg
+        return redirect('/')
         
 app.run(debug=True)
